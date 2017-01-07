33e65576215cef4434f0f2f2d832cedae553968e
FunThomas424242 <funthomas424242@gmail.com>
2017-01-07 21:19:24 +0100

    initial setup
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ src/com/github/funthomas424242/familienbuch/handlers/AboutHandler.java
@@ -0,0 +1,12 @@
+package com.github.funthomas424242.familienbuch.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class AboutHandler {
+	@Execute
+	public void execute(Shell shell) {
+		MessageDialog.openInformation(shell, "About", "Familienbuch");
+	}
+}
